e1df55d13bf53ddf120e40f182e34f28110f3982
Erik-Aron-Szabo <erik.aron.szabo@gmail.com>
2020-02-20 10:32:47 +0100

    Tests added (2) Menu different

---
+++ UnitTest/TestDeckShuffle.cs
@@ -0,0 +1,27 @@
+﻿using NUnit.Framework;
+using ModellingProjectGSE2;
+using System.Collections.Generic;
+using System;
+
+namespace UnitTest
+{
+    [TestFixture]
+    public class TestDeckShuffle
+    {
+        [TestCase]
+        public void TestShuffle()
+        {
+            Read reader = new ModellingProjectGSE2.Read();
+            List<Card> cardList1;
+            cardList1 = reader.LoadFromXMLParts();
+            List<Card> cardList2;
+            cardList2 = reader.LoadFromXMLParts();
+            Utility util = new Utility();
+            util.Shuffle(cardList2);
+
+            Assert.AreNotEqual(cardList1, cardList2);
+        }
+
+
+    }
+}
